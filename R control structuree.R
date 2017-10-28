7c064924fe7b10d825ed91832c654ddbd6108b1f
KunxiangMa <KunxiangMa@users.noreply.github.com>
2017-10-28 16:26:52 +0100

    to task 7

--- R control structuree.R
+++ R control structuree.R
@@ -25,5 +25,31 @@ z <- c(3,5,7)
 ifelse(x<4, y, z)
 
 # task 5
+x <- rnorm(10)
+# method 1
+ifelse(x < 0, 0, x)
+# method 2
+for (i in 1:10) {
+  if (x[i] < 0 ) {
+    x[i] <- 0
+  }
+}
+x
+# method 3
+x <- 1/2 * (x + abs(x))
+x
+
+# task 6
+x <- c(1,2,9)
+cumsum(x)
+# new method
+m <- x[1]
+for (i in 2:length(x)) {
+  m[i] <- m[i-1] + x[i]
+}
+print(m)
+
+# task 7
+
 
 
