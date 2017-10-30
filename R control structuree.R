cf224a5197aec2e69f3ba6efb60cd33e2c99eece
KunxiangMa <KunxiangMa@users.noreply.github.com>
2017-10-30 01:01:10 +0000

    change for task8

--- R control structuree.R
+++ R control structuree.R
@@ -60,11 +60,28 @@ x[i]
 
 # task 8
 n <- 10
+leng <- matrix(numeric(n*n), ncol =n)
 coords <- matrix(rnorm (2*n), ncol =2)
-plot(coords)
-for (i in ) {
-  lines(x=coords[1,i],y=coords[2,i])
+plot(x = coords[,1], y = coords[,2], type = "p")
+i <- 1
+j <- 1
+for(i in 1:10) {
+  for(j in i:10) {
+    segments(coords[i,1],coords[i,2],coords[j,1],coords[j,2])
+    div1 <- coords[j,1] - coords[i,1]
+    div2 <- coords[j,2] - coords[i,2]
+    leng[i,j] <- sqrt ( div1 ^ 2 +div2 ^ 2 )
+    leng[j,i] <- NA
+    if (i>0 & j>i) {
+      if (!is.na(leng[i,j-1]) & leng[i,j] < leng[i,j-1]) {
+        im <- i
+        jm <- j
+      } else {}
+    } else {}
+  }
 }
+segments(coords[im,1],coords[im,2],coords[jm,1],coords[jm,2],lwd = 4, col = "red")
+
 
 
 
