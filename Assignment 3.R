445bb02d29901a6a62005f9e63c03bab253d039e
KunxiangMa <KunxiangMa@users.noreply.github.com>
2017-11-28 23:40:09 +0000

    1

--- Assignment 3.R
+++ Assignment 3.R
@@ -32,19 +32,21 @@ circle(c(0,2), c(0,0), c(1, 0.5))
 
 # 1 c
 
-circle <- function(x,y,r,col,xlab){
+circle <- function(x,y,r,col,a,b){
   palette(rainbow(7))
   eqscplot(x, y, type="n", xlim=c(min(x)-1.5*r[which.min(x-r)], max(x)+1.5*r[which.max(x+r)]),
-           ylim=c(min(y)-1.5*r[which.min(y-r)], max(y)+1.5*r[which.max(y+r)]))
-  par(xlab = "a",)
+           ylim=c(min(y)-1.5*r[which.min(y-r)], max(y)+1.5*r[which.max(y+r)]),
+           xlab = a,ylab = b)
+  data <- cbind(x,y,r,col)
+  data <- data[order(data[,3], decreasing = TRUE),]
   for (i in 1:length(x)) {
-    polygon(x = x[i] + r[i] * sin(t), y = y[i] + r[i] * cos(t),col = col[i])
+    polygon(x = data[i,1] + data[i,3] * sin(t), y = data[i,2] + data[i,3] * cos(t),col = data[i,4])
   }
   leg <- as.factor(names(table(col)))
-  legend("bottomright", legend = leg, fill = leg )
+  legend("bottomright", legend = leg, fill = leg)
 }
 
-circle(health$HealthExpenditure,health$LifeExpectancy,sqrt(health$Population)/1e4
-       ,"health$Region,Health expenditure in $100's", "Life expectancy")
-class(health$HealthExpenditure)
+circle(health$HealthExpenditure,health$LifeExpectancy,sqrt(health$Population)/1e4,
+       health$Region, "health$Region,Health expenditure in $100's", "Life expectancy")
+
 
